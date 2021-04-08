9052936083f9e2495e143ac026edd41da1a967dc
Bean-jun <1342104001@qq.com>
2021-04-09 00:59:31 +0800

    # feat： 完成用户的注册，登录

--- blog/forms/account.py
+++ blog/forms/account.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.forms import ValidationError
 from blog.models import UserInfo
+from utils.encrypt import md5
 
 
 class BootStrapForm(object):
@@ -17,20 +19,79 @@ class RegisterForm(BootStrapForm, forms.ModelForm):
     password = forms.CharField(label='密码',
                                min_length=8,
                                max_length=32,
+                               error_messages={
+                                   'min_length': '密码不易太短',
+                                   'max_length': '密码不易太长',
+                               },
                                widget=forms.PasswordInput())
     
     confirm_password = forms.CharField(label='重复密码',
                                        min_length=8,
                                        max_length=32,
+                                       error_messages={
+                                            'min_length': '密码不易太短',
+                                            'max_length': '密码不易太长',
+                                        },
                                        widget=forms.PasswordInput())
 
     class Meta:
         model = UserInfo
         fields = ['username', 'email', 'password']
 
+    def clean_email(self):
+        """校验邮箱"""
+        email = self.cleaned_data['email']
+
+        exists = UserInfo.objects.filter(email=email).exists()
+        if exists:
+            raise ValidationError("该邮箱已注册")
+        
+        return email
+
+    def clean_password(self):
+        """加密密码"""
+        password = self.cleaned_data['password']
+        
+        return md5(password)
+
+    def clean_confirm_password(self):
+        """校验密码"""
+        confirm_password = self.cleaned_data['confirm_password']
+        password = self.cleaned_data['password']
+
+        if md5(confirm_password) != password:
+            raise ValidationError("两次密码不一致")
+
+        return confirm_password
+
 
 class LoginForm(BootStrapForm, forms.ModelForm):
     """用户登录校验"""
+
+    password = forms.CharField(label='密码',
+                               min_length=8,
+                               max_length=32,
+                               error_messages={
+                                   'min_length': '密码不易太短',
+                                   'max_length': '密码不易太长',
+                               },
+                               widget=forms.PasswordInput())
     class Meta:
         model = UserInfo
-        fields = ['username', 'email', 'password']
+        fields = ['email', 'password']
+
+    def clean_email(self):
+        """校验邮箱"""
+        email = self.cleaned_data['email']
+
+        exists = UserInfo.objects.filter(email=email).exists()
+        if not exists:
+            raise ValidationError("该邮箱未注册")
+        
+        return email
+
+    def clean_password(self):
+        """加密密码"""
+        password = self.cleaned_data['password']
+        
+        return md5(password)
--- blog/views/account.py
+++ blog/views/account.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
 from django.views import View
 from blog.forms.account import RegisterForm, LoginForm
+from blog.models import UserInfo
 
 
 
@@ -13,7 +16,18 @@ class RegisterView(View):
         return render(request, 'register.html', {'form': form})
 
     def post(self, request):
-        pass
+
+        form = RegisterForm(request.POST)
+
+        # 校验数据--数据格式问题--密码的一致性
+        if form.is_valid():
+            # 保存数据
+            form.instance.is_super = False  # 任何用户注册都是普通用户
+            form.save()
+
+            return JsonResponse({'code': 200})
+        
+        return JsonResponse({'code': 416, 'msg': form.errors})
 
 
 class LoginView(View):
@@ -25,4 +39,23 @@ class LoginView(View):
         return render(request, 'login.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = LoginForm(request.POST)
+
+        # 校验数据--邮箱存在问题--密码的一致性
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            user = UserInfo.objects.filter(email=email, password=password).first()
+            
+            if user:
+                # 用户登录成功，做session设置
+                request.session['user_id'] = user.id
+                request.session.set_expiry(60 * 60 * 24 * 7)
+
+                return redirect(reverse("blog:index"))
+            else:
+                # 用户密码错误
+                form.add_error('email', '邮箱或者密码错误')
+        
+        return render(request, 'login.html', {'form': form})
