51a76ec21f568cf0be7277a379b661f64c7c76b6
Bean-jun <1342104001@qq.com>
2021-04-23 12:32:26 +0800

    # feat: 功能有点多
    	1、调整前端管理员页面布局；
    	2、添加网易云音乐设置标签--前端
    	3、博客分类设置--前端
    	4、备案成功--添加备案

--- blog/models.py
+++ blog/models.py
@@ -12,12 +12,12 @@ class UserInfo(models.Model):
     bucket = models.CharField(max_length=128, verbose_name="用户文件存储桶")
     region = models.CharField(max_length=32, verbose_name="地区")
 
-    
+
 class Category(models.Model):
     """分类"""
     name = models.CharField(max_length=32, verbose_name="分类名称")
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
-    
+
 
 class Note(models.Model):
     """用户笔记表"""
@@ -26,16 +26,17 @@ class Note(models.Model):
     content = models.TextField(verbose_name="内容")
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     modify_datetime = models.DateTimeField(auto_now=True, verbose_name="最后修改时间")
-    top_image = models.ImageField(max_length=256, upload_to='note_image/', height_field='', width_field='', verbose_name="笔记快照")
+    top_image = models.ImageField(max_length=256, upload_to='note_image/', height_field='', width_field='',
+                                  verbose_name="笔记快照")
 
-  
+
 class Image(models.Model):
     """笔记图片表"""
     note = models.ForeignKey('Note', on_delete=models.CASCADE, verbose_name='笔记')
     image = models.ImageField(upload_to='note/', verbose_name="笔记图片")
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
-   
+
 class UserComment(models.Model):
     """用户评论"""
     user = models.ForeignKey('UserInfo', on_delete=models.CASCADE, verbose_name="用户")
@@ -46,3 +47,14 @@ class UserComment(models.Model):
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="评论时间")
     is_top = models.BooleanField(default=False, verbose_name="是否置顶")
 
+
+class Song(models.Model):
+    """博客歌单"""
+    PLAY_CHOICES = (
+        (1, '播放'),
+        (2, '暂停'),
+    )
+    user = models.ForeignKey('UserInfo', on_delete=models.CASCADE, verbose_name="用户")
+    song_id = models.IntegerField(verbose_name='歌单ID')
+    title = models.CharField(max_length=64, verbose_name="歌单标题")
+    is_play = models.BooleanField(choices=PLAY_CHOICES, default=2, verbose_name="是否首页播放")
