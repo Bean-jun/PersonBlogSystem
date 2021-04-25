a553f9ff6fe77c857fca56bb285c451505468ae5
Bean-jun <1342104001@qq.com>
2021-04-25 16:56:58 +0800

    # fix: 修复访客无法查看分类博客

--- blog/templatetags/category_navigate.py
+++ blog/templatetags/category_navigate.py
@@ -7,5 +7,5 @@ register = Library()
 @register.inclusion_tag('inclusion/category_navigate.html')
 def category_navigate(request):
     """分类导航栏"""
-    category_obj = Category.objects.filter(user=request.user, user__is_super=True)
+    category_obj = Category.objects.filter(user__is_super=True)
     return {'category_pre': category_obj[:5], 'category_end': category_obj[5:]}
